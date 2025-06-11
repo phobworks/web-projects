cc52badd513ae3bb11ffbe27a1eac422a39f72a3
phedwallace <phedwallace36th@live.com>
2025-06-11 20:17:35 +0100

    Committed IESL Project

---
+++ js/main.js
@@ -0,0 +1,49 @@
+/* Navbar  */
+const nav = document.querySelector(".nav__links");
+const openNavBtn = document.querySelector("#nav__toggle-open");
+const closeNavBtn = document.querySelector("#nav__toggle-close");
+
+const openNav = () => {
+  nav.style.display = "flex";
+  nav.classList.remove("reverse"); // Remove reverse class in case it exists
+  openNavBtn.style.display = "none";
+  closeNavBtn.style.display = "inline-block";
+};
+
+openNavBtn.addEventListener("click", openNav);
+
+const closeNav = () => {
+  // Add reverse animation class
+  nav.classList.add("reverse");
+
+  // Wait for the animation to finish before hiding the menu
+  nav.addEventListener(
+    "animationend",
+    () => {
+      nav.style.display = "none";
+      openNavBtn.style.display = "inline-block";
+      closeNavBtn.style.display = "none";
+    },
+    { once: true } // Ensures the event listener is removed after execution
+  );
+};
+
+closeNavBtn.addEventListener("click", closeNav);
+
+// Close nav on link click
+nav.querySelectorAll("li a").forEach((navLink) => {
+  navLink.addEventListener("click", closeNav);
+});
+
+
+// ========= Banner rating section Starts =========
+
+  const bannerRating = document.getElementById("bannerRating");
+
+    // Clone the star icon 4 more times (we already have 1 in HTML)
+    for (let i = 0; i < 4; i++) {
+        const clone = bannerRating.cloneNode(true);
+        bannerRating.parentNode.insertBefore(clone, bannerRating.nextSibling);
+    }
+
+    // ========= Banner rating section ends =========
